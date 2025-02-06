a377cc7b82dbe812dc2a97722311a248beaacf5d
Connor Kooistra <cd.kooistra@outlook.com>
2025-02-06 13:56:54 +0100

    add: update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,17 @@ from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import pyshorteners
+import validators
 
 router = APIRouter()
 
 class Item(BaseModel):
+    """
+    Item model representing a URL.
+
+    Attributes:
+        url (str): The URL string.
+    """
     url: str
 
 url_db = {}
@@ -17,13 +24,14 @@ def url_shortener(url: str) -> str:
     return s.tinyurl.short(url)
 
 
+
 @router.get("/", status_code= 200)
-def read_root():
+def list_keys():
     return JSONResponse({"keys": list(url_db.keys())}, status_code=200)
 
 @router.post("/", status_code=201)
 def shorten_url(item: Item):
-    if not item.url.startswith("http"):
+    if not validators.url(item.url):
         raise HTTPException(status_code=400, detail="error: Invalid URL")
 
     shorten_url = url_shortener(item.url)
@@ -31,21 +39,28 @@ def shorten_url(item: Item):
     return {"Short_url": shorten_url}
 
 
+@router.get("/{url_key}", status_code=301)
+def redirect_url(url_key: str):
+    if url_key not in url_db:
+        raise HTTPException(status_code=404, detail="error: URL not found")
+    
+    return JSONResponse(status_code=301, content={"Location": url_db[url_key]})
 
-# @app.get("/")
-# get keys, 200
 
-# @app.post("/")
-# post URL to shorten ->
-# 201, id
-# 400, error?
+@router.put("/{url_key}", status_code=200)
+def update_url(url_key: str, item: Item):
+    if url_key not in url_db:
+        raise HTTPException(status_code=404, detail="error: URL not found")
+    
+    # catch any other exception for return code 400? "error"
 
-# @app.delete("/")
-# 404 error?
+    url_db[url_key] = item.url
+    return {"updated url key": url_key}
 
-# @app.get("/{url_id}")
-# return redirect (301) to URL or 404 not found
-
-# @app.put("/{url_id}")
-
-# @app.delete("/{url_id}")
+@router.delete("/{url_key}", status_code=204)
+def delete_url(url_key: str):
+    if url_key not in url_db:
+        raise HTTPException(status_code=404, detail="error: URL not found")
+    
+    del url_db[url_key]
+    return {"deleted url key": url_key}
