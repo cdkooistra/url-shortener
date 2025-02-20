c25ed841f85ed4fe5f4f96efa8c85475cc2d0ce5
Connor Kooistra <cd.kooistra@outlook.com>
2025-02-20 16:14:48 +0100

    add: removed .env file, switched to docker secrets

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from app.routes import router as main_router
 from app.models import create_database
-from dotenv import load_dotenv
 
-load_dotenv()           # we should be passing secrets as env variables in a .env file
-                        # look at my example.env file for reference
 create_database()
 
 app = FastAPI()
--- auth/main.py
+++ auth/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
 from auth.models import create_database
-from dotenv import load_dotenv
 
-load_dotenv()           # we should be passing secrets as env variables in a .env file
-                        # look at my example.env file for reference
 create_database()
 
 app = FastAPI()
